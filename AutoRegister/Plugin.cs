3d4880afcf094ab425b8fc67a888d139b4c69fbc
brian91292 <8050523+brian91292@users.noreply.github.com>
2019-06-12 12:00:49 -0500

    🦀REGISTERING IS GONE🦀

---
+++ AutoRegister/Plugin.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Terraria;
+using Terraria.Localization;
+using TerrariaApi.Server;
+using TShockAPI;
+using TShockAPI.DB;
+using TShockAPI.Hooks;
+using static TShockAPI.GetDataHandlers;
+
+namespace AutoRegister
+{
+    /// <summary>
+    /// The main plugin class should always be decorated with an ApiVersion attribute. The current API Version is 1.25
+    /// </summary>
+    [ApiVersion(2, 1)]
+    public class Plugin : TerrariaPlugin
+    {
+        /// <summary>
+        /// The name of the plugin.
+        /// </summary>
+        public override string Name => "AutoRegister";
+
+        /// <summary>
+        /// The version of the plugin in its current state.
+        /// </summary>
+        public override Version Version => new Version(1, 0, 0);
+
+        /// <summary>
+        /// The author(s) of the plugin.
+        /// </summary>
+        public override string Author => "brian91292";
+
+        /// <summary>
+        /// A short, one-line, description of the plugin's purpose.
+        /// </summary>
+        public override string Description => "A Tshock plugin to automatically register a new server-side character if one doesn't already exist for a user.";
+
+        /// <summary>
+        /// The plugin's constructor
+        /// Set your plugin's order (optional) and any other constructor logic here
+        /// </summary>
+        public Plugin(Main game) : base(game)
+        {
+        }
+
+        public static void Log(string msg,
+                        [CallerFilePath] string file = "",
+                        [CallerMemberName] string member = "",
+                        [CallerLineNumber] int line = 0)
+        {
+            Console.WriteLine($"AutoRegister::{member}({line}): {msg}");
+        }
+
+        /// <summary>
+        /// Performs plugin initialization logic.
+        /// Add your hooks, config file read/writes, etc here
+        /// </summary>
+        public override void Initialize()
+        {
+            ServerApi.Hooks.ServerJoin.Register(this, OnServerJoin);
+            ServerApi.Hooks.NetGreetPlayer.Register(this, OnGreetPlayer, 420);
+        }
+
+        private Dictionary<int, string> tmpPasswords = new Dictionary<int, string>();
+        /// <summary>
+        /// Tell the player their password if the account was newly generated
+        /// </summary>
+        /// <param name="args"></param>
+        void OnGreetPlayer(GreetPlayerEventArgs args)
+        {
+            if (tmpPasswords.TryGetValue(args.Who, out string newPass))
+            {
+                try
+                {
+                    TShock.Players[args.Who].SendSuccessMessage($"New server-side character created successfully! Your password is \"{newPass}\".");
+                    TShock.Players[args.Who].SendSuccessMessage($"Contact an admin if you lose access to this account, or forget your password.");
+                }
+                catch { }
+                tmpPasswords.Remove(args.Who);
+            }
+        }
+
+        /// <summary>
+        /// Fired when a new user joins the server.
+        /// </summary>
+        /// <param name="args"></param>
+        void OnServerJoin(JoinEventArgs args)
+        {
+            if (TShock.ServerSideCharacterConfig.Enabled)
+            {
+                var player = TShock.Players[args.Who];
+
+                // Get the user using a combo of their UUID/name, as this is what's required for uuid login to function it seems
+                var users = TShock.Users.GetUsers().Where(u => u.UUID == player.UUID && (u.Name == player.Name));
+                if (users.Count() == 0)
+                {
+                    Log($"Creating new user for {player.Name}...");
+                    tmpPasswords[args.Who] = Convert.ToBase64String(Guid.NewGuid().ToByteArray()).Substring(0, 10);
+                    // If the user didn't exist, automatically create a new user based on their uuid.
+                    TShock.Users.AddUser(new User(
+                        player.Name,
+                        BCrypt.Net.BCrypt.HashPassword(tmpPasswords[args.Who].Trim()),
+                        player.UUID,
+                        TShock.Config.DefaultRegistrationGroupName,
+                        DateTime.UtcNow.ToString("s"),
+                        DateTime.UtcNow.ToString("s"),
+                        ""));
+
+                    Log("Success!");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Performs plugin cleanup logic
+        /// Remove your hooks and perform general cleanup here
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ServerApi.Hooks.ServerJoin.Deregister(this, OnServerJoin);
+                ServerApi.Hooks.NetGreetPlayer.Deregister(this, OnGreetPlayer);
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Mod Info
+AutoRegister is a TShock plugin created to automatically register new users on SSC enabled servers. 
+
+# How it works
+When a new user joins a server AutoRegister checks to see if there is already a user present with their name/uuid. If there isn't, a new user is automatically created for them. The password which is generated is then posted in chat for them to either save, or change.
